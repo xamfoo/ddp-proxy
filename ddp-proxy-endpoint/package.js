045119a00f813f02d21489879d0064e8450ed8ff
xamfoo <xamfoo@all.repo>
2015-09-23 18:44:51 +0800

    Bump to 0.2.0

--- ddp-proxy-endpoint/package.js
+++ ddp-proxy-endpoint/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'xamfoo:ddp-proxy-endpoint',
-  version: '0.1.0',
+  version: '0.2.0',
   // Brief, one-line summary of the package.
   summary: 'Server package to support xamfoo:ddp-proxy',
   // URL to the Git repository containing the source code for this package.
